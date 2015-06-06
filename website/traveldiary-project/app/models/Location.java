620118d74ebb8a6e06a46125b32edd22d2ac41f2
Chetan Basuray <urwithchetan@gmail.com>
2015-06-06 11:52:35 +0200

    adding some getters and setters

--- website/traveldiary-project/app/models/Location.java
+++ website/traveldiary-project/app/models/Location.java
@@ -7,8 +7,39 @@ public class Location {
 
     private String name;
     private String description;
-
     private double latitude;
     private double longtitude;
 
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongtitude() {
+        return longtitude;
+    }
+
+    public void setLongtitude(double longtitude) {
+        this.longtitude = longtitude;
+    }
 }
