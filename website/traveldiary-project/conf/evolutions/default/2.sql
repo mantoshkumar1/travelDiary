a8bfe566a342d12c5643e7d06a7ef5461f6ad224
Chetan Basuray <urwithchetan@gmail.com>
2015-06-07 11:06:45 +0200

    committing insert queries and changes in date formats for database tables

--- website/traveldiary-project/conf/evolutions/default/2.sql
+++ website/traveldiary-project/conf/evolutions/default/2.sql
@@ -1,27 +1,32 @@
 # --- !Ups
 
 --inserting data into KEYWORDS table
-INSERT INTO Keyword VALUES (0,'BEACH');
-INSERT INTO Keyword VALUES (1,'SUNNY');
-INSERT INTO Keyword VALUES (2,'MUSEUM');
-INSERT INTO Keyword VALUES (3,'VOLLEY BALL');
-INSERT INTO Keyword VALUES (4,'WATER');
-INSERT INTO Keyword VALUES (5,'SUN');
+INSERT INTO Keyword VALUES (1,'BEACH');
+INSERT INTO Keyword VALUES (2,'SUNNY');
+INSERT INTO Keyword VALUES (3,'MUSEUM');
+INSERT INTO Keyword VALUES (4,'VOLLEY BALL');
+INSERT INTO Keyword VALUES (5,'WATER');
+INSERT INTO Keyword VALUES (6,'SUN');
 
 -- inserting Vacation for debugging
 
 INSERT INTO Role
-VALUES (0,'User');
+VALUES (1,'User');
+
+INSERT INTO Role
+VALUES (2,'Administrator');
 
 INSERT INTO Location
-VALUES (0,'name','description',0.0,0.0);
+VALUES (1,'name','description',0.0,0.0);
 
 INSERT INTO User
-VALUES (0,'jd','John','Doe','SOMEHASHVALUE',0);
+VALUES (1,'jd','John','Doe','SOMEHASHVALUE',1);
 
 INSERT INTO Vacation
-VALUES (0, 'Some Vacation','Some Description', 2.5, date('now'), date('now'), 0, 0);
+VALUES (1, 'Some Vacation','Some Description', 2.5, date('now'), date('now'), 1, 1);
+
+INSERT INTO activity VALUES (1,	'Museum',	'Some Museum', date('now'), date(now),  1,  1);
+INSERT INTO activity VALUES (2,	'Stadium',	'Some Stadium', date('now'), date(now),  1,  1);
+INSERT INTO activity VALUES (3,	'Park',	'Some Park', date('now'), date(now),  1,  1);
 
 # --- !Downs
-
-
