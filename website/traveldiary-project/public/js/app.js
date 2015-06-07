a56c0fbe90e4e2fc3596d1c5a9f5610a07582af1
Ulrike Niemann <ulrike.niemann@tum.de>
2015-06-07 20:55:01 +0200

    removal of search-keywords

--- website/traveldiary-project/public/js/app.js
+++ website/traveldiary-project/public/js/app.js
@@ -141,6 +141,15 @@
             }
         };
 
+        $scope.removeKeyword = function(newKeyword){
+            if(newKeyword != undefined) {
+                if(containsKeyword($scope.searchList, newKeyword)){
+                    var index = $scope.searchList.indexOf(newKeyword);
+                    $scope.searchList.splice(index, 1);
+                }
+            }
+        };
+
         var containsKeyword = function(list, keyword){
             var found = false;
             for(var i = 0; i < list.length; i++) {
@@ -150,6 +159,21 @@
                 }
             }
             return found;
+        };
+
+        $scope.getSearchFilter = function($viewValue) {
+            console.log("entered searchFilter");
+            var matching = [];
+            console.log($scope.keywordList);
+            console.log($scope.keywordList.length);
+            for (var i=0; i < $scope.keywordList.length; i++) {
+                console.log($scope.keywordList[i].keyword);
+                if ($scope.keywordList[i].keyword.toLowerCase().indexOf($viewValue.toLowerCase()) != -1){
+                    matching.push($scope.stuffs[i]);
+                }
+            }
+            console.log(matching);
+            return matching;
         }
     }]);
 
