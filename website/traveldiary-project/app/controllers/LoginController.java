a0d57ebddaea8d71445231799a97418f9431dcfa
Mantosh Kumar <mantoshkumar1@gmail.com>
2015-07-01 18:54:35 +0200

    login_not_complete_yet

--- website/traveldiary-project/app/controllers/LoginController.java
+++ website/traveldiary-project/app/controllers/LoginController.java
@@ -1,24 +1,37 @@
 package controllers;
 
+import play.db.jpa.Transactional;
 import play.mvc.Controller;
+import play.mvc.Result;
 
 public class LoginController extends Controller {
-    public static void login(String email, String password) {
+
+    @Transactional
+    public static Result login(String email, String passwordHash) {
         models.User user = null;
+
         user = models.User.findByEmail(email);
+        System.out.println("inside login controller");
+        System.out.println(user);
         if(user != null){
-            if(password == user.getPasswordHash()) {
+            if(passwordHash == user.getPasswordHash()) {
                 String temp =  user.getId()+ "";
                 session("user", temp);
                 //String userId = session("user");
+                flash("success", "Welcome to your account");
                 redirect("/");
+                return ok("Welcome");
             }
         }
+        return unauthorized("Oops, invalid email/password");
     }
 
     // Clears the session(client cookie)
-    public static void logout(){
+    @Transactional
+    public static Result logout() {
         session("user", null);
+        flash("success", "Successfully Logout");
         redirect("/");
+        return ok("Bye");
     }
 }
--- website/traveldiary-project/public/javascripts/login.js
+++ website/traveldiary-project/public/javascripts/login.js
@@ -8,10 +8,18 @@ var App = angular.module("travelDiary");
 App.controller('loginController', ['$scope', '$state', 'User', '$http', function($scope, $state, User, $http){
 
     $scope.login = function () {
-        $http.post('api/login', {email:$scope.email,pass: $scope.pass}).success(function(data){
+        $http.post('api/login', {email:$scope.email,passwordHash: $scope.passwordHash}).success(function(data){
             //take care of promise
         });
         //after login successful go here
+        //$state.go('main.index');
+    }
+
+    $scope.logout = function () {
+        $http.post('api/logout', {}).success(function(data){
+            //take care of promise
+        });
+        //after logout successful go here
         $state.go('main.index');
     }
 }]);
