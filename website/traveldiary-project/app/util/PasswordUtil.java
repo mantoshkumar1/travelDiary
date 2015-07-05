5dff449e007cf95a98bec8ab75b453f647a85cb0
Mantosh Kumar <mantoshkumar1@gmail.com>
2015-07-05 18:53:02 +0200

    enabling encryption again

--- website/traveldiary-project/app/util/PasswordUtil.java
+++ website/traveldiary-project/app/util/PasswordUtil.java
@@ -7,16 +7,13 @@ public class PasswordUtil {
 
     //encoding password with OpenBSD-style Blowfish password hashing function
     public static String calculateHashString(String plainPassword) {
-        // String pw_hash = BCrypt.hashpw(plainPassword, BCrypt.gensalt());
-
-
-        return plainPassword;
+        String pw_hash = BCrypt.hashpw(plainPassword, BCrypt.gensalt());
+        return pw_hash;
     }
 
     //compare plain password with hashed password using OpenBSD-style Blowfish password hashing function
     public static boolean comparePassword(String candidate_password, String stored_hash) {
         //if password matches, returns true otherwise false
-        //return BCrypt.checkpw(candidate_password, stored_hash);
-        return candidate_password.equals(stored_hash);
+        return BCrypt.checkpw(candidate_password, stored_hash);
     }
 }
